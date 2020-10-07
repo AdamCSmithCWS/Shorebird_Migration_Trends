d997d3408c9213deeb87fd8894e383722395034e
AdamCSmithCWS <adam.smith2@canada.ca>
2020-10-07 19:22:39 -0400

    flailing...

--- models/AnnualSlopeSeasonalGAM_ZIP2.R
+++ models/AnnualSlopeSeasonalGAM_ZIP2.R
@@ -5,7 +5,7 @@ model
 {
 for(i in 1:ncounts){
 
-	elambda1[i] <- alpha[strat[i]] + ste[site[i],strat[i]] + beta[strat[i]]*(yr[i]-midyear) + year_effect[yr[i],strat[i]] + gam.sm_season[i,strat[i]] + noise[i]  ### common intercept, varying slopes, so that the site effect accounts for all of the variation in abundance.
+	elambda1[i] <- alpha[strat[i]] + ste[site[i],strat[i]] + beta[strat[i]]*(yr[i]-midyear) + year_effect[yr[i],strat[i]] + gam.sm_season[i] + noise[i]  ### common intercept, varying slopes, so that the site effect accounts for all of the variation in abundance.
 	
 	noise[i] ~ dt(0,taunoise[strat[i]],nu[strat[i]])
 	#noise[i] ~ dnorm(0,taunoise)
@@ -45,57 +45,57 @@ for(i in 1:ncounts){
   ###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
   # Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
   
-  taugam_season <- 1/pow(sdgam_season,2)
-  sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-  
-  
-  for(k in 1:nknots_season){
-    B_season[k] ~ dnorm(0,taugam_season)
-    
-    taugam_season_s[k] <- 1/pow(sdgam_season_s[k],2)
-    sdgam_season_s[k] ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-    
-    for(s in 1:nstrata){
-      
-      b_season[k,s] ~ dnorm(0,taugam_season_s[k])
-      
-      beta_season[k,s] <- b_season[k,s]+B_season[k]
-    }
-  }
-  
-  
-  
-  #strata smooths
-  for(s in 1:nstrata){
-    
-    gam.sm_season[1:ncounts,s] <-	season_basis %*% beta_season[1:nknots_season,s]
-    ##### derived parameters to visualize the smooth
-    
-    vis.sm_season[1:ndays,s] <-	season_basispred %*% beta_season[1:nknots_season,s]
-    mn_sm_season[s] <- mean(vis.sm_season[,s])
-    
-  }#s
+  # taugam_season <- 1/pow(sdgam_season,2)
+  # sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+  # 
+  # 
+  # for(k in 1:nknots_season){
+  #   B_season[k] ~ dnorm(0,taugam_season)
+  #   
+  #   taugam_season_s[k] <- 1/pow(sdgam_season_s[k],2)
+  #   sdgam_season_s[k] ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+  #   
+  #   for(s in 1:nstrata){
+  #     
+  #     b_season[k,s] ~ dnorm(0,taugam_season_s[k])
+  #     
+  #     beta_season[k,s] <- b_season[k,s]+B_season[k]
+  #   }
+  # }
+  # 
+  # 
+  # 
+  # #strata smooths
+  # for(s in 1:nstrata){
+  #   
+  #   gam.sm_season[1:ncounts,s] <-	season_basis %*% beta_season[1:nknots_season,s]
+  #   ##### derived parameters to visualize the smooth
+  #   
+  #   vis.sm_season[1:ndays,s] <-	season_basispred %*% beta_season[1:nknots_season,s]
+  #   mn_sm_season[s] <- mean(vis.sm_season[,s])
+  #   
+  # }#s
   
 #   
-# ###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
-# # Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
-# 
-# taugam_season <- 1/pow(sdgam_season,2)
-# sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-# 
-# 
-# for(k in 1:nknots_season){
-#   beta_season[k] ~ dnorm(0,taugam_season)
-# }
-# 
-# gam.sm_season <-	season_basis %*% beta_season
-# 
-# 
-# ##### derived parameters to visualize the smooth
-# 
-# vis.sm_season <-	season_basispred %*% beta_season
-# 
-# mn_sm_season <- mean(vis.sm_season)
+###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
+# Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
+
+taugam_season <- 1/pow(sdgam_season,2)
+sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+
+
+for(k in 1:nknots_season){
+  beta_season[k] ~ dnorm(0,taugam_season)
+}
+
+gam.sm_season <-	season_basis %*% beta_season
+
+
+##### derived parameters to visualize the smooth
+
+vis.sm_season <-	season_basispred %*% beta_season
+
+mn_sm_season <- mean(vis.sm_season)
 
 
 
@@ -139,13 +139,13 @@ for(y in 1:nyears){
 PSI ~ dbeta(1,1) #mean site-level zip parameter
 ZIP <- logit(PSI)
 tau_zip_js <- 1/pow(sd_zip_js,2)
-sd_zip_js ~ dt(0, 0.5, 4)T(0,) 
+sd_zip_js ~ dt(0, 0.2, 4)T(0,) #half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1
 
   for(s in 1:nstrata){
     alpha[s] ~ dnorm(0,1)
 
   for (j in 1:nsites[s]){
-    ste[j,s]~dt(0,tausite,nu_site)
+    ste[j,s]~dnorm(0,tausite)
     
     zip_js[j,s] ~ dnorm(0,tau_zip_js)
     zip[j,s] <- zip_js[j,s] + ZIP
@@ -155,11 +155,11 @@ sd_zip_js ~ dt(0, 0.5, 4)T(0,)
 
 tausite <- 1/pow(sdsite,2)
 sdsite ~ dt(0, 1, 4)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 3
-nu_site ~ dgamma(2,0.2) #
+#nu_site ~ dgamma(2,0.2) #
 
-retrans_j <- (0.5*(1/tausite))/nu_ret_j
+retrans_j <- (0.5*(1/tausite))
 
-nu_ret_j <- (1.422*nu_site^0.906)/(1+(1.422*nu_site^0.906)) #approximate retransformation to equate a t-distribution to a normal distribution - see appendix of Link et al. 2020 BBS model selection paper
+#nu_ret_j <- (1.422*nu_site^0.906)/(1+(1.422*nu_site^0.906)) #approximate retransformation to equate a t-distribution to a normal distribution - see appendix of Link et al. 2020 BBS model selection paper
 
 # 
 # 
@@ -200,7 +200,7 @@ alpha_m <- mean(alpha[1:nstrata])
 for(y in 1:nyears){
   for(s in 1:nstrata){
     for(j in 1:nsites[s]){
-      n_sj[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans[s])* psi[j,s] #site-level predictions including strata-level yearly smooths
+      n_sj[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season + retrans[s])* psi[j,s] #site-level predictions including strata-level yearly smooths
       # n_sj_a1[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans_a1[s])* psi #site-level predictions including only the normal noise component (ignoring the t-distributed error)
       # n_sj_a2[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s])* psi #site-level predictions excluding the log-normal retransformation completely
     }#j
@@ -212,7 +212,7 @@ for(y in 1:nyears){
      # n_s_scaled2[s,y] <- exp(alpha[s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans[s] + retrans_j)* psi #stratum predictions including strata-level yearly smooths and on a common scale (visualisation only)
      # 
       }#s
-  N[y] <- exp(alpha_m + B*(y-midyear) + YE[y] + retrans_m + retrans_j)*PSI #continental predictions including only the hyperparameter smooth
+  N[y] <- exp(alpha_m + B*(y-midyear) + YE[y] + mn_sm_season + retrans_m + retrans_j)*PSI #continental predictions including only the hyperparameter smooth
   N_comp[y] <- mean(n_s[1:nstrata,y]) #continental predictions 
   # N_sc[y] <- mean(n_s_scaled[1:nstrata,y]) #coh
   # N_sc2[y] <- mean(n_s_scaled2[1:nstrata,y]) #c
--- models/AnnualSlopeSeasonalGAM_ZIP3.R
+++ models/AnnualSlopeSeasonalGAM_ZIP3.R
@@ -5,15 +5,15 @@ model
 {
 for(i in 1:ncounts){
 
-	elambda1[i] <-  ste[site[i],strat[i]] + beta[strat[i]]*(yr[i]-midyear) + year_effect[yr[i],strat[i]] + gam.sm_season[i,strat[i]] + noise[i]  ### common intercept, varying slopes, so that the site effect accounts for all of the variation in abundance.
-	#alpha[strat[i]] +
+	elambda1[i] <- ste[site[i],strat[i]] + beta[strat[i]]*(yr[i]-midyear) + year_effect[yr[i],strat[i]] + gam.sm_season[i] + noise[i]  ### common intercept, varying slopes, so that the site effect accounts for all of the variation in abundance.
+	
 	noise[i] ~ dt(0,taunoise[strat[i]],nu[strat[i]])
 	#noise[i] ~ dnorm(0,taunoise)
 	log(lambda1[i]) <- elambda1[i]
 	
 	
 	lambda[i] <- lambda1[i]*z[i] + 0.00001 ## hack required for JAGS -- otherwise 'incompatible'-error
-	z[i] ~ dbern(psi)#psi[year[i]]) #psi = proportion of non-zeros for each year
+	z[i] ~ dbern(psi[site[i],strat[i]])#psi[year[i]]) #psi = proportion of non-zeros for each year
 	### how tomodel the zip component...at what scale do the zeros vary? sites, strata, years? probably sites given the crazy count distributions at some sites, but maybe just overall, given the fact that many sites have extreme variations
 	
 	
@@ -22,11 +22,10 @@ for(i in 1:ncounts){
 
 }#i
 
-  psi ~ dbeta(1,1)
-  
+ 
   
   for(s in 1:nstrata){
-
+   
       # sdnoise <- 1/pow(taunoise,0.5)
       # taunoise ~ dscaled.gamma(0.5,50)
       taunoise[s] <- 1/pow(sdnoise[s],2)
@@ -46,60 +45,61 @@ for(i in 1:ncounts){
   ###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
   # Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
   
-  taugam_season <- 1/pow(sdgam_season,2)
-  sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-  
-  B_season[1] <- sum(B_season[2:nknots_season])*-1 #sum to zero constraint to aid identifiability
-  
-  for(k in 2:nknots_season){
-    B_season[k] ~ dnorm(0,taugam_season)
-  }
-  for(k in 1:nknots_season){
-   
-    taugam_season_s[k] <- 1/pow(sdgam_season_s[k],2)
-    sdgam_season_s[k] ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-    
-    for(s in 1:nstrata){
-      
-      b_season[k,s] ~ dnorm(0,taugam_season_s[k])
-      
-      beta_season[k,s] <- b_season[k,s]+B_season[k]
-    }
-  }
-  
-  
-  
-  #strata smooths
-  for(s in 1:nstrata){
-    
-    gam.sm_season[1:ncounts,s] <-	season_basis %*% beta_season[1:nknots_season,s]
-    ##### derived parameters to visualize the smooth
-    
-    vis.sm_season[1:ndays,s] <-	season_basispred %*% beta_season[1:nknots_season,s]
-    mn_sm_season[s] <- mean(vis.sm_season[,s])
-    
-  }#s
+  # taugam_season <- 1/pow(sdgam_season,2)
+  # sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+  # 
+  # 
+  # for(k in 1:nknots_season){
+  #   B_season[k] ~ dnorm(0,taugam_season)
+  #   
+  #   taugam_season_s[k] <- 1/pow(sdgam_season_s[k],2)
+  #   sdgam_season_s[k] ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+  #   
+  #   for(s in 1:nstrata){
+  #     
+  #     b_season[k,s] ~ dnorm(0,taugam_season_s[k])
+  #     
+  #     beta_season[k,s] <- b_season[k,s]+B_season[k]
+  #   }
+  # }
+  # 
+  # 
+  # 
+  # #strata smooths
+  # for(s in 1:nstrata){
+  #   
+  #   gam.sm_season[1:ncounts,s] <-	season_basis %*% beta_season[1:nknots_season,s]
+  #   ##### derived parameters to visualize the smooth
+  #   
+  #   vis.sm_season[1:ndays,s] <-	season_basispred %*% beta_season[1:nknots_season,s]
+  #   mn_sm_season[s] <- mean(vis.sm_season[,s])
+  #   
+  # }#s
   
 #   
-# ###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
-# # Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
-# 
-# taugam_season <- 1/pow(sdgam_season,2)
-# sdgam_season ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
-# 
-# 
-# for(k in 1:nknots_season){
-#   beta_season[k] ~ dnorm(0,taugam_season)
-# }
-# 
-# gam.sm_season <-	season_basis %*% beta_season
-# 
-# 
-# ##### derived parameters to visualize the smooth
-# 
-# vis.sm_season <-	season_basispred %*% beta_season
-# 
-# mn_sm_season <- mean(vis.sm_season)
+###########COMPUTING GAMs for seasonal patterns hierarchical smooth across strata
+# Addapted from Crainiceanu, C. M., Ruppert, D. & Wand, M. P. (2005). Bayesian Analysis for Penalized Spline Regression Using WinBUGS. Journal of Statistical Softare, 14 (14), 1-24.
+
+taugam_season <- 1/pow(sdgam_season,2)
+sdgam_season ~ dt(0, 0.5, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1.0
+
+
+
+  beta_season[1] <- sum(beta_season[2:nknots_season])*-1
+
+
+for(k in 2:nknots_season){
+  beta_season[k] ~ dnorm(0,taugam_season)
+}
+
+gam.sm_season <-	season_basis %*% beta_season
+
+
+##### derived parameters to visualize the smooth
+
+vis.sm_season <-	season_basispred %*% beta_season
+
+mn_sm_season <- mean(vis.sm_season)
 
 
 
@@ -107,9 +107,9 @@ for(i in 1:ncounts){
 
 
 tau_beta <- 1/pow(sd_beta,2)
-sd_beta ~ dt(0, 1, 4)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 3.0
+sd_beta ~ dt(0, 0.1, 4)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 3.0
 
-B ~ dnorm(0,100) #regularizing prior putting 95% of the prior with absolute value of trends < 20%/year
+B ~ dnorm(0,1000) #regularizing prior putting 95% of the prior with absolute value of trends < 5%/year
   for(s in 1:nstrata){
     
     b[s] ~ dnorm(0,tau_beta)
@@ -118,8 +118,8 @@ B ~ dnorm(0,100) #regularizing prior putting 95% of the prior with absolute valu
 
 
 ##############Year Effects
-#tau_year <- 1/pow(sd_year,2) #variance in continental year effects, controls shrinkage towards the line
-#sd_year ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 2
+tau_year <- 1/pow(sd_year,2) #variance in continental year effects, controls shrinkage towards the line
+sd_year ~ dt(0, 1, 10)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 2
 
 for(s in 1:nstrata){
   tau_ye[s] <- 1/pow(sd_ye[s],2) #variance in strata-level departures from the continental year-effects - further shrinkage
@@ -127,22 +127,33 @@ for(s in 1:nstrata){
   
 }
 for(y in 1:nyears){
-  #YE[y] ~ dnorm(0,tau_year)
+  YE[y] ~ dnorm(0,tau_year)
   for(s in 1:nstrata){
   ye[y,s] ~ dnorm(0,tau_ye[s])
-    year_effect[y,s] <- ye[y,s]#+YE[y]
+    year_effect[y,s] <- YE[y]+ye[y,s]
       }
 }
 
 
 ######################
 ## site effects
-for(s in 1:nstrata){
-  #alpha[s] ~ dnorm(0,1)
+
+
+
+PSI ~ dbeta(1,1) #mean site-level zip parameter
+ZIP <- logit(PSI)
+tau_zip_js <- 1/pow(sd_zip_js,2)
+sd_zip_js ~ dt(0, 0.2, 4)T(0,) #half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 1
+
+  for(s in 1:nstrata){
+    #alpha[s] ~ dnorm(0,1)
+
   for (j in 1:nsites[s]){
-    #ste[j,s]~dt(0,tausite,nu_site)
-    ### try this next
     ste[j,s]~dnorm(0,tausite)
+    
+    zip_js[j,s] ~ dnorm(0,tau_zip_js)
+    zip[j,s] <- zip_js[j,s] + ZIP
+    logit(psi[j,s]) <- zip[j,s]
   }#j
 }#s
 
@@ -150,7 +161,7 @@ tausite <- 1/pow(sdsite,2)
 sdsite ~ dt(0, 1, 4)T(0,) # half-t prior on sd (chung et al. 2013) DOI: 10.1007/S11336-013-9328-2 places ~95% of the prior < 3
 #nu_site ~ dgamma(2,0.2) #
 
-retrans_j <- (0.5*(1/tausite))#/nu_ret_j
+retrans_j <- (0.5*(1/tausite))
 
 #nu_ret_j <- (1.422*nu_site^0.906)/(1+(1.422*nu_site^0.906)) #approximate retransformation to equate a t-distribution to a normal distribution - see appendix of Link et al. 2020 BBS model selection paper
 
@@ -193,7 +204,7 @@ retrans_m <- mean(retrans[1:nstrata])
 for(y in 1:nyears){
   for(s in 1:nstrata){
     for(j in 1:nsites[s]){
-      n_sj[j,s,y] <- exp(ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans[s])* psi #site-level predictions including strata-level yearly smooths
+      n_sj[j,s,y] <- exp(ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season + retrans[s])* psi[j,s] #site-level predictions including strata-level yearly smooths
       # n_sj_a1[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans_a1[s])* psi #site-level predictions including only the normal noise component (ignoring the t-distributed error)
       # n_sj_a2[j,s,y] <- exp(alpha[s] + ste[j,s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s])* psi #site-level predictions excluding the log-normal retransformation completely
     }#j
@@ -205,7 +216,7 @@ for(y in 1:nyears){
      # n_s_scaled2[s,y] <- exp(alpha[s] + beta[s]*(y-midyear) + year_effect[y,s] + mn_sm_season[s] + retrans[s] + retrans_j)* psi #stratum predictions including strata-level yearly smooths and on a common scale (visualisation only)
      # 
       }#s
-  N[y] <- exp(B*(y-midyear) +  retrans_m + retrans_j) #YE[y] +#continental predictions including only the hyperparameter smooth
+  N[y] <- exp(B*(y-midyear) + YE[y] + mn_sm_season + retrans_m + retrans_j)*PSI #continental predictions including only the hyperparameter smooth
   N_comp[y] <- mean(n_s[1:nstrata,y]) #continental predictions 
   # N_sc[y] <- mean(n_s_scaled[1:nstrata,y]) #coh
   # N_sc2[y] <- mean(n_s_scaled2[1:nstrata,y]) #c
