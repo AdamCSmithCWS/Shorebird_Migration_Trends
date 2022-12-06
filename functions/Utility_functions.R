71b4b406c86250b3056e1f631a2b4f72ada0f870
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2022-12-06 15:26:00 -0500

    geographic patterns in trends supplement

--- functions/Utility_functions.R
+++ functions/Utility_functions.R
@@ -393,7 +393,7 @@ ItoT <- function(inds = NSamples,
       
       ttt1 <- left_join(ttt1,ttt2,by = ".draw")
       ttt1 <- ttt1 %>% 
-        mutate(td = (t_c - t)*(1/nyrs)) %>% 
+        mutate(td = (t_c - t)*(1/nyrs)) %>% # difference in log(abundance ratios): symetrical about 0
         ungroup() %>% 
         group_by(region) %>% 
         summarise(centered_log_trend = mean(td),
